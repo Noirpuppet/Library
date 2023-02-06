6a200130b36e6df8d57c4475713794f146d67697
Noirpuppet <chenin2808@gmail.com>
2023-02-06 23:03:04 +0530

    branching

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const year = new Date().getFullYear();
+document.getElementById('year').innerText = year;
+
 const addBtn = document.createElement('button');
 addBtn.classList.add('addBtn');
 addBtn.textContent = '+'
@@ -21,9 +24,46 @@ document.querySelector('.closeBtn')
     });
 
 
+let library = [];
+
+class Book {
+    constructor(title, author, page, read) {
+        this.title;
+        this.author;
+        this.page;
+        this.read;
+    }
+}
+
+// const addBookToLibrary = (e) => {
+//     e.perventdefault();
+//     let book = new Book(title, author, page, read);
+//     library.push(book);
+//     document.querySelector('form').reset();
+//   }
+
+document.querySelector('.addBook')
+    .addEventListener('click', addBookToLibrary);
+
 function addBookToLibrary() {
 
+    const addCard = document.getElementById('cardBox');
+    const newCard = document.createElement('div');
+    newCard.classList.add('card');
+    newCard.innerHTML = `<div>title</div>
+                        <div>author</div>
+                        <div>page</div>
+                        <div>read</div>`;
 
+    const newEdit = document.createElement('div');
+    newEdit.innerText = 'Edit';
+    newEdit.classList.add('edit');
+    const newRemove = document.createElement('div');
+    newRemove.innerText = 'Remove';
+    newRemove.classList.add('remove');
 
+    addCard.appendChild(newCard);
+    newCard.appendChild(newEdit);
+    newCard.appendChild(newRemove);
+};
 
-  };
