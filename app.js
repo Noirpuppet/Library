5554b1fa7dd956a8cfe5355a2152d55d19a0e418
Noirpuppet <chenin2808@gmail.com>
2023-02-21 23:47:01 +0530

    added library and class constructure

--- app.js
+++ app.js
@@ -23,47 +23,59 @@ document.querySelector('.closeBtn')
         .style.transform = 'translate(-50%, -50%) scale(0)';
     });
 
-
 let library = [];
 
 class Book {
-    constructor(title, author, page, read) {
-        this.title;
-        this.author;
-        this.page;
-        this.read;
+    constructor (title, author, page, read) {
+        this.title = title;
+        this.author = author;
+        this.page = page;
+        this.read = read;
     }
 }
 
-// const addBookToLibrary = (e) => {
-//     e.perventdefault();
-//     let book = new Book(title, author, page, read);
-//     library.push(book);
-//     document.querySelector('form').reset();
-//   }
+const addBookToLibrary = (e) => {
+    e.preventDefault();
 
-document.querySelector('.addBook')
-    .addEventListener('click', addBookToLibrary);
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const page = document.getElementById('page').value;
+    const read = document.getElementById('read').checked;
+    document.querySelector('form').reset();
 
-function addBookToLibrary() {
+    const books = new Book(title, author, page, read);
+    library.push(books);
+    displayBook();
+    console.log(library)
+}
 
-    const addCard = document.getElementById('cardBox');
-    const newCard = document.createElement('div');
-    newCard.classList.add('card');
-    newCard.innerHTML = `<div>title</div>
-                        <div>author</div>
-                        <div>page</div>
-                        <div>read</div>`;
+document.querySelector('form').addEventListener('submit', addBookToLibrary);
 
-    const newEdit = document.createElement('div');
-    newEdit.innerText = 'Edit';
-    newEdit.classList.add('edit');
-    const newRemove = document.createElement('div');
-    newRemove.innerText = 'Remove';
-    newRemove.classList.add('remove');
+const displayBook = () => {
+    const bookContainer = document.getElementById('cardBox');
 
-    addCard.appendChild(newCard);
-    newCard.appendChild(newEdit);
-    newCard.appendChild(newRemove);
+    while (bookContainer.firstChild) {
+        bookContainer.removeChild(bookContainer.firstChild);
+      };
+
+    library.forEach(book => {
+        const addCard = document.getElementById('cardBox');
+        const newCard = document.createElement('div');
+        newCard.classList.add('card');
+        newCard.innerHTML = `<div>${book.title}</div>
+                            <div>${book.author}</div>
+                            <div>${book.page} pages</div>
+                            <div>${book.read}</div>`;
+
+        const newEdit = document.createElement('div');
+        newEdit.innerText = 'Edit';
+        newEdit.classList.add('edit');
+        const newRemove = document.createElement('div');
+        newRemove.innerText = 'Remove';
+        newRemove.classList.add('remove');
+
+        addCard.appendChild(newCard);
+        newCard.appendChild(newEdit);
+        newCard.appendChild(newRemove);
+    });
 };
-
